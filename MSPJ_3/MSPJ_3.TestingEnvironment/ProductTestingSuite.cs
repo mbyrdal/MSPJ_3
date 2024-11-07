f73b6f78ad0cd413d4013ed6bc41eeeb9942ef3a
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-07 11:30:16 +0100

    Create Product Test Suite
    For various testing purposes when working with the Product class...
    
    - Object initialization
    - Constructor usage
    - Properties & fields
    - Methods

---
+++ MSPJ_3/MSPJ_3.TestingEnvironment/ProductTestingSuite.cs
@@ -0,0 +1,26 @@
+namespace MSPJ_3.TestingEnvironment
+{
+    public class ProductTestingSuite
+    {
+        /// <summary>
+        /// hej med dig jeg hedder kaj og jeg er meget frej
+        /// </summary>
+        [Fact]
+        public void Test_Product_CreateObjectUsingDefaultConstructor()
+        {
+            // Arrange
+            // No input arrangements needed since we are testing the default constructor.
+            // However, if we have a default value when using the constructor, then we can prepare those...
+
+            // string defaultName = "DefaultName";
+            // int defaultPrice = 1000;
+
+            // Act
+            // var myProduct = new Product();
+
+            // Assert.IsType<Product>(myProduct);
+            // Assert.Equal(defaultName, myProduct.Name);
+            // Assert.Equal(defaultPrice, myProduct.Price);
+        }
+    }
+}
