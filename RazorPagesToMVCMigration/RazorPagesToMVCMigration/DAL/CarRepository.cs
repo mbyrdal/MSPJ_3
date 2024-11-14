408e3b6d489308569e52f02fdd774a793c5d9808
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-14 12:14:06 +0100

    .
    .

--- RazorPagesToMVCMigration/RazorPagesToMVCMigration/DAL/CarRepository.cs
+++ RazorPagesToMVCMigration/RazorPagesToMVCMigration/DAL/CarRepository.cs
@@ -2,8 +2,37 @@
 
 namespace RazorPagesToMVCMigration.DAL
 {
+    ConfigurationHelper cfgHelper = new ConfigurationHelper;
     public class CarRepository : ICarRepository
     {
+        public void CreateCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void DeleteCar(string VINNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car GetCarByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
