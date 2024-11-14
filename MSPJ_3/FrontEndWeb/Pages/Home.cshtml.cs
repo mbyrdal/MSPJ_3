08fe65aeb07a1003c370410185b291e4a4f161fc
Páll Sigurbjörnsson Larsen <10000164@ucn.dk>
2024-11-14 09:11:43 +0100

    qwer

---
+++ MSPJ_3/FrontEndWeb/Pages/Home.cshtml.cs
@@ -0,0 +1,13 @@
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FrontEndWeb.Pages
+{
+    public class HomeModel : PageModel
+    {
+        public void OnGet()
+        {
+            // No logic needed here for static content
+        }
+    }
+}
