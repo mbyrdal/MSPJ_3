caa0d7188758af033781c9cbc3eb4ff9ecc575cd
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2024-11-14 12:17:11 +0100

    implemented interfaces into repositories

--- RazorPagesToMVCMigration/RazorPagesToMVCMigration/DAL/AccountRepository.cs
+++ RazorPagesToMVCMigration/RazorPagesToMVCMigration/DAL/AccountRepository.cs
@@ -2,7 +2,36 @@
 
 namespace RazorPagesToMVCMigration.DAL
 {
-    public class AccountRepository
+    public class AccountRepository : IAccountRepository
     {
+        public void CreateAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAccount(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetAccountsByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAllAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
