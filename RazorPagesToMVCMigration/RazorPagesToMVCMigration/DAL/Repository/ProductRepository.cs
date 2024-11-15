d5cb4ccd4f58fc2514282e039986d5c33d8870a2
JuliaRafnUCN <152272464+JuliaRafnUCN@users.noreply.github.com>
2024-11-15 12:07:41 +0100

    fixed double to decimal

--- RazorPagesToMVCMigration/RazorPagesToMVCMigration/DAL/Repository/ProductRepository.cs
+++ RazorPagesToMVCMigration/RazorPagesToMVCMigration/DAL/Repository/ProductRepository.cs
@@ -41,7 +41,7 @@ namespace RazorPagesToMVCMigration.DAL.Repository
                                 OEM = reader.GetString(0), // Column 1, VINNumber ...
                                 VINNumber = reader.GetString(1),
                                 Name = reader.GetString(2),
-                                Price = reader.GetDouble(3),
+                                Price = reader.GetDecimal(3),
                                 DateAvailable = reader.GetDateTime(4),
                                 Notes = reader.GetString(5)
                             };
