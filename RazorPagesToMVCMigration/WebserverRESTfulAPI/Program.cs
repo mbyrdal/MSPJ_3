8a903d0e0e66f868406d49c746e78ab1ae2c48bd
Mads Byrdal Nielsen <mbyrdal9@gmail.com>
2024-11-19 00:18:58 +0100

    Finalized working API example...

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/Program.cs
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Program.cs
@@ -1,6 +1,9 @@
+using ServiceAPI.BusinessLogic.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<ProductService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
