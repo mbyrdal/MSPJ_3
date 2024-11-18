faa4e9cfe69e699f261e176b5b82d6167063f056
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-18 13:16:52 +0100

    .
    .

---
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace RazorPagesToMVCMigration.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public List<string>? ListOfProducts { get; set; }
+        public double TotalPrice { get; set; }
+        public DateTime DateSold { get; set; }
+    }
+}
---
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace RazorPagesToMVCMigration.Models
+{
+    public class Product
+    {
+        public string? OEM { get; set; }
+        public string? VINNumber { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTime DateAvailable { get; set; }
+        public string? Notes { get; set; }
+    }
+}
