92d3d9266f30448ba77313f9c89da7216b2a7a14
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-18 10:00:02 +0100

    Added TestingEnvironment to MVCMigraiton

---
+++ RazorPagesToMVCMigration/TestingEnvironment/CarTestingSuite.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSPJ_3.TestingEnvironment
+{   /// <summary>
+    /// 
+    /// </summary>
+
+    public class CarTestingSuite //Should be renamed to CustomerTestingSuite but im scared it deletes the entire document if i do it again:(
+    {
+        /// <summary>
+        /// NEEDED
+        /// </summary>
+        [Fact]
+        public void Car_ShouldInitializeWithCorrectProperties()
+        {
+            // Arrange & Act
+            // var car = new Car
+            //  { 
+            // vinNumber = "ABCDE12345F123456",
+            // manufacturer = "Honda",
+            // model = "Civic",
+            // productionYear = 1997,
+            // mileage= 123500
+            //  };
+
+            // Assert
+            // Assert.Equal("ABCDE12345F123456", car.vinNumber)
+            // Assert.Equal("Honda", car.nimNumber, car.
+            // Assert.Equal("Civic", car.model)
+            // Assert.Equal(1997, car.productionYear)
+            // Assert.Equal(123500, car.mileage)
+        }
+
+        /// <summary>
+        /// We want the VIN number to follow a specific format,
+        /// 17 char for standard VIN numbers.
+        /// </summary>
+        [Fact]
+        public void Car_VinNumber_ShouldBe17Characters()
+        {
+            // Arrange
+            // var car = new Car { vinNumber = "1HGCM82633A123456" };
+
+            // Act
+            // int vinLength = car.vinNumber.Length;
+
+            // Assert
+            // Assert.Equal(17, vinLength);
+        }
+
+        /// <summary>
+        /// We check if the productionYear is valid, meaning any year between now and 1886.
+        /// 1886 is the year the first car was made.
+        /// </summary>
+        [Fact]
+        public void Car_ProductionYearShouldBeValid()
+        {
+            // Arrange
+            // var car = new Car { productionYear = 2022 };
+
+            // Act & Assert
+            // Assert.InRange(car.productionYear, 1886, DateTime.Now.Year);
+        }
+
+        [Fact]
+        public void Car_MilageHasToBePositive()
+        {
+            // Arrange
+            // var car = new Car { mileage = 10000 };
+
+            // Act & Assert
+            // Assert.True(car.mileage >= 0, "Mileage should be positives.");
+        }
+    }
+}
---
+++ RazorPagesToMVCMigration/TestingEnvironment/ProductTestingSuite.cs
@@ -0,0 +1,52 @@
+namespace MSPJ_3.TestingEnvironment
+{
+    public class ProductTestingSuite
+    {
+        /// <summary>
+        /// This method tests the default constructor implementation of the Product class.<br/>
+        /// Default/initial values for properties are compared to constructor-assigned ones. <br/>
+        /// Finally, the expected vs actual object types are also compared (expecting a Person class object).
+        /// </summary>
+        [Fact]
+        public void Test_Product_CreateUsingDefaultConstructor()
+        {
+            // Arrange
+            // No input arrangements needed since we are testing the default constructor.
+            // However, if we have a default value when using the constructor, then we can prepare those...
+
+            // string defaultName = "DefaultName";
+            // int defaultPrice = 1000;
+
+            // Act
+            // var myProduct = new Product();
+
+            // Assert
+            // Assert.IsType<Product>(myProduct); // Expected type is Product
+            // Assert.Equal(defaultName, myProduct.Name); // Expected value is "DefaultName"
+            // Assert.Equal(defaultPrice, myProduct.Price); // Expected value is 1000
+        }
+
+        /// <summary>
+        /// This method tests a custom constructor implementation of the Product class.<br/>
+        /// Arranged values are compared to constructor-assigned ones.<br/>
+        /// Finally, the expected vs actual object types are also compared (expecting a Person class object).
+        /// </summary>
+        [Fact]
+        public void Test_Product_CreateUsingConstructor()
+        {
+            // Arrange
+            // string Name = "Name";
+            // int Price = 2500;
+            // var DateListed = new DateTime(2020, 10, 11); // October 11th, 2024
+
+            // Act
+            // var myProduct = new Product(Name, Price, DateListed);
+
+            // Assert
+            // Assert.IsType<Product>(myProduct); // Expected type for myProduct is Product
+            // Assert.Equal(Name, myProduct.Name); // Expected value is "DefaultName"
+            // Assert.Equal(Price, myProduct.Price); // Expected value is 1000
+            // Assert.Equal(DateListed, myProduct.Date); // Expected value is "11/10/2020"
+        }
+    }
+}
