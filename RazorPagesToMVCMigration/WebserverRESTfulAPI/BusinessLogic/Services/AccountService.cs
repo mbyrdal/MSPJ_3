4963c7132ad6398b1e4ab2b8ff5911a40331fe94
Mads Byrdal Nielsen <mbyrdal9@gmail.com>
2024-11-21 22:59:15 +0100

    Removed Service objects ... to be replaced by {Model}Control classes

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/BusinessLogic/Services/AccountService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ServiceAPI.BusinessLogic.Services
-{
-    public class AccountService
-    {
-    }
-}
--- RazorPagesToMVCMigration/WebserverRESTfulAPI/BusinessLogic/Services/GuestService.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ServiceAPI.BusinessLogic.Services
-{
-    public class GuestService
-    {
-    }
-}
