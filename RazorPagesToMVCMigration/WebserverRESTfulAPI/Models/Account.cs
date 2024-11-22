8123b07ea6d4e547cefdb557c363aa3a4ae631c9
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-22 10:58:05 +0100

    Added password hashing logic to Account class

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/Account.cs
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/Account.cs
@@ -1,14 +1,33 @@
 ﻿namespace ServiceAPI.Models
 {
-    public class Account : Customer
+    public class Account : Guest
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string PhoneNum { get; set; } = string.Empty;
-        public string Password { get; set; }
+        private string _passwordHash;
         public List<string>? ListOfOrders { get; set; }
-
+        /// <summary>
+        /// This method verifies a plaintext password against a hashed password.
+        /// </summary>
+        /// <param name="plainTextPassword">The plaintext password to be hashed, provided as input parameter.</param>
+        /// <returns>True or False, depending on whether there is a match between the password and hash.</returns>
+        public bool VerifyAccountPassword(string plainTextPassword)
+        {
+            // Compare the plain text password to the hash
+            return BCrypt.Net.BCrypt.Verify(plainTextPassword, _passwordHash);
+        }
+        /// <summary>
+        /// This method hashes a plaintext password using the bcrypt algorithm.
+        /// </summary>
+        /// <param name="plainTextPassword">The plaintext password to be hashed, provided as input parameter.</param>
+        /// <returns>The hashed password.</returns>
+        private string HashAccountPassword(string plainTextPassword)
+        {
+            // Generate a salt and hash password.
+            return BCrypt.Net.BCrypt.HashPassword(plainTextPassword);
+        }
         public Account()
         {
             FirstName = string.Empty;
@@ -16,7 +35,7 @@
             Address = "InTheMiddleOfNowhere Street 50";
             PhoneNum = "12345678";
             Email = "default@email.com";
-            Password = "123456abcdef!@#¤%_XYZ";
+            _passwordHash = HashAccountPassword("123456abcdef!@#¤%_XYZ");
         }
 
         public Account(string fName, string lName, string address, string pnum, string email, string pw)
@@ -26,7 +45,7 @@
             Address = address;
             PhoneNum = pnum;
             Email = email;
-            Password = pw;
+            _passwordHash = HashAccountPassword(pw);
         }
     }
 }
