7aa523c9bbaa336e8a844a7eef6b06567d80b1c1
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-22 12:48:00 +0100

    Update AccountControl.cs

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/BusinessLogic/AccountControl.cs
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/BusinessLogic/AccountControl.cs
@@ -1,31 +1,70 @@
 ﻿using ServiceAPI.BusinessLogic.Interfaces;
+using ServiceAPI.DatabaseAccess;
 using ServiceAPI.Models;
+using System.Diagnostics;
 
 namespace ServiceAPI.BusinessLogic
 {
     public class AccountControl : IAccountControl
     {
-        // 
+        private readonly DbAccount _dbAccountAccess;
+        public AccountControl(DbAccount dbAccountAccess)
+        {
+            _dbAccountAccess = dbAccountAccess;
+        }
+        public Account GetAccountByEmail(string email)
+        {
+            Account accountPlaceholder = null;
+            try
+            {
+                accountPlaceholder = _dbAccountAccess.GetByIdentifier(email);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+            return accountPlaceholder;
+        }
+        public List<Account> GetAllAccounts()
+        {
+            List<Account> allAccounts = new List<Account>();
+            try
+            {
+                allAccounts = _dbAccountAccess.GetAllEntities();
+            }
+            catch (Exception ex)
+            {
+                allProducts = null;
+                Debug.WriteLine(ex.Message);
+            }
+            return allAccounts;
+        }
         public bool AddAccount(Account account)
         {
-            throw new NotImplementedException();
+            bool accountExists = false;
+            bool wasAccountInserted = false;
+            int numberOfRowsInserted;
+            try
+            {
+                accountExists = _dbAccountAccess.AccountExists(account.Email);
+                if (!accountExists)
+                {
+                    numberOfRowsInserted = _dbAccountAccess.CreateEntity(account);
+                    wasAccountInserted = (numberOfRowsInserted == 1);
+                }
+            }
+            catch (Exception ex)
+            {
+                account = null;
+                Debug.WriteLine(ex.Message);
+            }
+            return wasAccountInserted;
         }
 
         public bool DeleteAccount(Account account)
         {
             throw new NotImplementedException();
         }
-
-        public Account GetAccountByEmail(string email)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<Account> GetAllAccounts()
-        {
-            throw new NotImplementedException();
-        }
-
         public bool UpdateAccount(Account account)
         {
             throw new NotImplementedException();
