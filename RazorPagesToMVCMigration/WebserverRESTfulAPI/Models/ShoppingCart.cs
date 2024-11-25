41eba993407b1d4e0fb42ba45e8e2b928d8c568f
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-25 12:59:05 +0100

    Create ShoppingCart.cs
    + Configured Session state in Program.cs

---
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/ShoppingCart.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceAPI.Models
+{
+    public class ShoppingCart
+    {
+        public List<Product>? Items { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public ShoppingCart()
+        {
+            TotalPrice = 0;
+        }
+
+        public ShoppingCart(List<Product> items)
+        {
+            Items = items;
+            TotalPrice = items.Sum(item => item.Price);
+        }
+
+        public ShoppingCart(List<Product> items, decimal totalPrice)
+        {
+            Items = items;
+            TotalPrice = totalPrice;
+        }
+    }
+}
