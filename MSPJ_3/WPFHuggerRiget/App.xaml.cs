8f230aa03f3b3b6497a40dbada4121af354dc8ca
Schnappsen <121798300+Schnappsen@users.noreply.github.com>
2024-11-26 09:28:15 +0100

    admin wpf client

---
+++ MSPJ_3/WPFHuggerRiget/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFHuggerRiget
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
