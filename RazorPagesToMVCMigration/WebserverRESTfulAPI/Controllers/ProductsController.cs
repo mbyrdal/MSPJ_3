46aab82e2c27341dcc29f5b7e464865987c37856
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-26 11:12:14 +0100

    Update ProductsController.cs

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/Controllers/ProductsController.cs
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Controllers/ProductsController.cs
@@ -152,7 +152,7 @@ namespace ServiceAPI.Controllers
 
         // TODO: trim and remove existingProduct logic since _productControl.UpdateProduct handles existing product issue already.
         // PUT: https://localhost:7134/api/Products/ID
-        [HttpPut("{ID:int}")]
+        [HttpPut("dto/{ID:int}")]
         public IActionResult UpdateProductDTO(int ID, [FromBody] ProductViewModel updatedProduct)
         {
             if (updatedProduct == null)
