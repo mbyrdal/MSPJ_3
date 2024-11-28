4c942937370ec332454f3e8f763b7bb0ba99534f
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-28 13:49:03 +0100

    Update Product.cs

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/Product.cs
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/Models/Product.cs
@@ -32,8 +32,6 @@ namespace ServiceAPI.Models
         public string ItemDescription { get; set; } = string.Empty;
 
         public bool ItemAvailable { get; set; }
-        public Car? ProductCarOrigin { get; set; }
-        public CarPart? ProductCarPartOrigin { get; set; }
 
         public Product() { }
 
