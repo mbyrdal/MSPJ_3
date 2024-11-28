a322ade2b3b47ae7daa360cf51b29a53cfa80283
Mads Byrdal Nielsen <31509859+mbyrdal@users.noreply.github.com>
2024-11-28 13:16:57 +0100

    Update ProductViewModel.cs

--- RazorPagesToMVCMigration/WebserverRESTfulAPI/DTOs/ProductViewModel.cs
+++ RazorPagesToMVCMigration/WebserverRESTfulAPI/DTOs/ProductViewModel.cs
@@ -42,7 +42,7 @@ namespace ServiceAPI.DTOs
 
         [Required]
         [ForeignKey("CarID")]
-        public CarViewModel ProductCarOrigin { get; set; }
+        public Car ProductCarOrigin { get; set; }
 
         public ProductViewModel() { }
 
