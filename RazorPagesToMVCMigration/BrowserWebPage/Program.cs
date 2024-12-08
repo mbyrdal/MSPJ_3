d1bf0304a0650b8e359a6df32d3131b395f957a5
Mads Byrdal Nielsen <mbyrdal9@gmail.com>
2024-12-08 21:53:42 +0100

    ?!

--- RazorPagesToMVCMigration/BrowserWebPage/Program.cs
+++ RazorPagesToMVCMigration/BrowserWebPage/Program.cs
@@ -1,53 +1,3 @@
-/*using BrowserWebPage.Data;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using ServiceAPI.BusinessLogic;
-using ServiceAPI.BusinessLogic.Interfaces;
-using ServiceAPI.DatabaseAccess;
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-//builder.Services.AddDbContext<ApplicationDbContext>(options =>
-//    options.UseSqlServer(connectionString));
-//builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-builder.Services.AddScoped<DbProduct>();
-builder.Services.AddScoped<IProductControl, ProductControl>();
-
-//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-//    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
-builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
-*/
 using ServiceAPI.BusinessLogic.Interfaces;
 using ServiceAPI.BusinessLogic;
 using ServiceAPI.DatabaseAccess;
@@ -70,6 +20,12 @@ builder.Services.AddSession(options =>
 // Other service registrations (DbProduct, ProductControl, etc.)
 builder.Services.AddScoped<DbProduct>();
 builder.Services.AddScoped<IProductControl, ProductControl>();
+
+// Add Authentication services with JWT bearer token support
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.
+});
 
 var app = builder.Build();
 
